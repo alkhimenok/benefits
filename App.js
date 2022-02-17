c5926120ded7357510d579a73ce2f2564c45d8aa
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-18 01:56:36 +0300

    add: navigation

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar'
 import { View, Text, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 export default function App() {
+	const DiscountScreen = ({ route, navigation }) => {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Discount</Text>
+			</View>
+		)
+	}
+	const FavoritesScreen = ({ navigation }) => {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Favorites</Text>
+			</View>
+		)
+	}
+	const AccountScreen = ({ navigation }) => {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Account</Text>
+			</View>
+		)
+	}
+
+	const Tab = createBottomTabNavigator()
+
 	return (
-		<View>
-			<Text>Hello World!</Text>
-			<Button title='Нажми на меня' />
-			<StatusBar style='auto' />
-		</View>
+		<NavigationContainer>
+			<Tab.Navigator
+				backBehavior={'history'}
+				// screenOptions={{ title: 'Скидки' }}
+			>
+				<Tab.Screen
+					name='discount'
+					component={DiscountScreen}
+					options={{ title: 'Скидки' }}
+				/>
+				<Tab.Screen
+					name='favorites'
+					component={FavoritesScreen}
+					options={{ title: 'Избраное' }}
+				/>
+				<Tab.Screen
+					name='account'
+					component={AccountScreen}
+					options={{ title: 'Акаунт' }}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
 	)
 }
