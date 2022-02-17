a8a6ad7311963ec9839435106a743f1a0c822aa9
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-17 23:41:29 +0300

    add: assets

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<View>
 			<Text>Hello World!</Text>
 			<Button title='Нажми на меня' />
 			<StatusBar style='auto' />
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-})
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				require.resolve('babel-plugin-module-resolver'),
+				{
+					root: ['./src/'],
+					alias: {
+						src: './src',
+						assets: './src/assets',
+						components: './src/components',
+						constants: './src/constants'
+					}
+				}
+			]
+		]
+	}
+}
