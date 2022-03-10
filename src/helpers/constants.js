ca8fb26936435276e4ed36c06f5ae80684b42483
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-10 14:55:58 +0300

    add: helpers

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,4 @@
-export const optionList = [
+const optionList = [
 	{
 		title: 'Все скидки',
 		icon: { name: 'important', position: 'start' },
@@ -40,3 +40,57 @@ export const optionList = [
 	// 	isDisabled: true
 	// }
 ]
+
+const imageSourceList = {
+	odin: require('assets/images/services/clothes/odin.png'),
+	fasol: require('assets/images/services/educationAndHobbies/fasol.png'),
+	onlineClass: require('assets/images/services/educationAndHobbies/onlineClass.png'),
+	robolab: require('assets/images/services/educationAndHobbies/robolab.png'),
+	skyeng: require('assets/images/services/educationAndHobbies/skyeng.png'),
+	dominosPizza: require('assets/images/services/foodAndDrink/dominosPizza.png'),
+	embargo: require('assets/images/services/foodAndDrink/embargo.png'),
+	myata: require('assets/images/services/foodAndDrink/myata.png'),
+	sprintBar: require('assets/images/services/foodAndDrink/sprintBar.png'),
+	evo: require('assets/images/services/health/evo.png'),
+	voka: require('assets/images/services/health/voka.png'),
+	undefinedCard: require('assets/images/services/undefinedCard.png'),
+	emptyFavorites: require('assets/images/services/emptyFavorites.png'),
+	emptyAvatar: require('assets/images/profile/emptyAvatar.png')
+}
+
+const iconsSourceList = {
+	account: require('assets/icons/account.png'),
+	alert: require('assets/icons/alert.png'),
+	back: require('assets/icons/back.png'),
+	bottom: require('assets/icons/bottom.png'),
+	connection: require('assets/icons/connection.png'),
+	continue: require('assets/icons/continue.png'),
+	default: require('assets/icons/default.png'),
+	denied: require('assets/icons/denied.png'),
+	discount: require('assets/icons/discount.png'),
+	email: require('assets/icons/email.png'),
+	facebook: require('assets/icons/facebook.png'),
+	favorites: require('assets/icons/favorites.png'),
+	favoritesFill: require('assets/icons/favoritesFill.png'),
+	fine: require('assets/icons/fine.png'),
+	important: require('assets/icons/important.png'),
+	importantFill: require('assets/icons/importantFill.png'),
+	instagram: require('assets/icons/instagram.png'),
+	left: require('assets/icons/left.png'),
+	location: require('assets/icons/location.png'),
+	more: require('assets/icons/more.png'),
+	navigation: require('assets/icons/navigation.png'),
+	options: require('assets/icons/options.png'),
+	phone: require('assets/icons/phone.png'),
+	reload: require('assets/icons/reload.png'),
+	right: require('assets/icons/right.png'),
+	search: require('assets/icons/search.png'),
+	telegram: require('assets/icons/telegram.png'),
+	top: require('assets/icons/top.png'),
+	unavailable: require('assets/icons/unavailable.png'),
+	vk: require('assets/icons/vk.png'),
+	youtube: require('assets/icons/youtube.png'),
+	close: require('assets/icons/close.png')
+}
+
+export { optionList, imageSourceList, iconsSourceList }
