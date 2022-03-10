7de8b2ce956edc16c718a90b929babf03bbbc71d
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-10 14:57:38 +0300

    fix(ElLabel): icon component

--- src/components/elements/ElLabel.js
+++ src/components/elements/ElLabel.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components/native'
+import ElIcon from './ElIcon'
+import ElText from './ElText'
 import UNITS from 'styles/units'
-import FONTS from 'styles/fonts'
 import COLORS from 'styles/colors'
 
 const ElLabel = ({ title, icon, isLarge }) => {
 	const isContainsIcon = !!icon.name
-	const iconBeginning = icon.position === 'start'
-	const { height, paddingHorizontal, fontSize } = getLabelOptions(
-		isLarge,
-		isContainsIcon
-	)
+	const iconBeginning = icon.position !== 'end'
+	const { height, paddingHorizontal } = getLabelOptions(isLarge, isContainsIcon)
 
 	return (
 		<Label
@@ -20,9 +18,23 @@ const ElLabel = ({ title, icon, isLarge }) => {
 			paddingHorizontalSize={paddingHorizontal}
 		>
 			{isContainsIcon && (
-				<IconWrapper iconBeginning={iconBeginning}>icon</IconWrapper>
+				<IconWrapper iconBeginning={iconBeginning}>
+					<ElIcon
+						variation={'empty'}
+						name={icon.name}
+						width={'16px'}
+						height={'16px'}
+						color={COLORS.light}
+					/>
+				</IconWrapper>
 			)}
-			<Title fontSize={fontSize}>{title}</Title>
+			<ElText
+				variation={isLarge ? 'text2' : 'text3'}
+				weight={600}
+				color={COLORS.light}
+			>
+				{title}
+			</ElText>
 		</Label>
 	)
 }
@@ -40,11 +52,6 @@ const IconWrapper = styled.Text`
 	${({ iconBeginning }) =>
 		`margin-${iconBeginning ? 'right' : 'left'}: ${UNITS.baseX2}`}
 `
-const Title = styled.Text`
-	font-family: ${FONTS.primarySemiBold};
-	font-size: ${({ fontSize }) => fontSize};
-	color: ${COLORS.light};
-`
 
 const getLabelOptions = (isLarge, isContainsIcon) => {
 	return {
@@ -52,8 +59,7 @@ const getLabelOptions = (isLarge, isContainsIcon) => {
 			(isLarge && UNITS.baseX10) ||
 			(isContainsIcon && UNITS.baseX8) ||
 			UNITS.baseX6,
-		paddingHorizontal: (isLarge && UNITS.baseX4) || UNITS.baseX2,
-		fontSize: (isLarge && UNITS.baseX3_5) || UNITS.baseX3
+		paddingHorizontal: (isLarge && UNITS.baseX4) || UNITS.baseX2
 	}
 }
 
