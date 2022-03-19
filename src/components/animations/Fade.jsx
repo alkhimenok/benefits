cee88f19d5d2a96eabc86b07365a32a6074444c8
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-19 18:36:13 +0300

    add(Fade): animations

---
+++ src/components/animations/Fade.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Animated } from 'react-native'
+
+const Fade = ({ duration, children }) => {
+	const fadeAnim = useRef(new Animated.Value(0)).current
+
+	useEffect(() => {
+		Animated.timing(fadeAnim, {
+			toValue: 1,
+			duration: duration,
+			useNativeDriver: true
+		}).start()
+	}, [fadeAnim])
+
+	return (
+		<Animated.View style={{ flex: 1, opacity: fadeAnim }}>
+			{children}
+		</Animated.View>
+	)
+}
+
+Fade.defaultProps = {
+	duration: 1000,
+	children: <></>
+}
+Fade.propTypes = {
+	duration: PropTypes.number,
+	children: PropTypes.node
+}
+
+export default Fade
