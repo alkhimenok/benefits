2f99a0c4f71de03ee484d9b18f516fba06fdb1f9
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-19 23:59:56 +0300

    fix: screen units

--- src/screens/discount/Discount.jsx
+++ src/screens/discount/Discount.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
 import Fade from 'components/animations/Fade'
 
 const Discount = () => {
 	return (
 		<Fade>
-			<View
-				style={{
-					flex: 1,
-					backgroundColor: 'red'
-					// transform: [{ translateY: -100 }]
-				}}
-			></View>
+			<></>
 		</Fade>
 	)
 }
--- src/screens/favorites/emptyFavorites/styledEmptyFavorites.js
+++ src/screens/favorites/emptyFavorites/styledEmptyFavorites.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native'
 import { sizeInProportion } from 'helpers/utils'
-import UNITS from 'styles/units'
+import { UNITS_PX } from 'styles/units'
 
 const EmptyFavoritesWrapper = styled.View`
 	flex: 1;
@@ -8,7 +8,7 @@ const EmptyFavoritesWrapper = styled.View`
 	justify-content: center;
 `
 const EmptyFavoritesImageWrapper = styled.View`
-	margin: 0 0 ${UNITS.baseX6} 0;
+	margin: 0 0 ${UNITS_PX.baseX6} 0;
 	${() => {
 		const { width, height } = sizeInProportion(210, 160, true)
 
@@ -16,7 +16,7 @@ const EmptyFavoritesImageWrapper = styled.View`
 	}}
 `
 const EmptyFavoritesTitleWrapper = styled.View`
-	margin: 0 0 ${UNITS.baseX3} 0;
+	margin: 0 0 ${UNITS_PX.baseX3} 0;
 `
 const EmptyFavoritesSubtitleWrapper = styled.View``
 
