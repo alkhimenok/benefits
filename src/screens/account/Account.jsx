7f08f6de770cb0a10b84d1f6d91b32890ebced30
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-24 22:43:32 +0300

    add: screen wrapper

--- src/screens/account/Account.jsx
+++ src/screens/account/Account.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Fade from 'components/animations/Fade'
+import Screen from 'components/common/Screen'
 
 const Account = () => {
 	return (
 		<Fade>
-			<></>
+			<Screen>
+				<></>
+			</Screen>
 		</Fade>
 	)
 }
