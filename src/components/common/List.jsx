984c15546beaf965842b5ae63889f5d624d73d3b
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-05-06 01:30:33 +0300

    fix: list props

--- src/components/common/List.jsx
+++ src/components/common/List.jsx
@@ -1,19 +1,29 @@
-import { useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components/native'
 
 const List = ({
 	data,
 	renderItem,
-	listEmptyComponent,
+	listHeader,
+	listFooter,
+	listEmpty,
 	contentContainerStyle,
 	isReturnScrollToStart,
-	isHorizontal
+	isHorizontal,
+	onRefresh
 }) => {
+	const [refresh, setRefresh] = useState(false)
 	const scroll = useRef()
 
 	const scrollToItem = (index, viewOffset) => {
 		scroll.current.scrollToIndex({ animated: true, index, viewOffset })
+	}
+
+	const handleRefresh = () => {
+		setRefresh(true)
+		onRefresh()
+		setRefresh(false)
 	}
 
 	useEffect(() => isReturnScrollToStart && scrollToItem(0), [])
@@ -23,12 +33,16 @@ const List = ({
 			ref={scroll}
 			data={data}
 			renderItem={({ item, index }) => renderItem(item, index, scrollToItem)}
-			ListEmptyComponent={listEmptyComponent}
+			ListHeaderComponent={listHeader}
+			ListFooterComponent={listFooter}
+			ListEmptyComponent={listEmpty}
 			keyExtractor={(item, index) => item + index}
 			contentContainerStyle={[{ flexGrow: 1 }, contentContainerStyle]}
 			horizontal={isHorizontal}
 			showsHorizontalScrollIndicator={false}
 			showsVerticalScrollIndicator={false}
+			refreshing={refresh}
+			onRefresh={handleRefresh}
 		/>
 	)
 }
@@ -37,19 +51,25 @@ const ListWrapper = styled.FlatList``
 
 List.defaultProps = {
 	data: [],
-	renderItem: <></>,
-	listEmptyComponent: <></>,
+	renderItem: () => null,
+	listHeader: <></>,
+	listFooter: <></>,
+	listEmpty: <></>,
 	contentContainerStyle: {},
 	isReturnScrollToStart: false,
-	isHorizontal: false
+	isHorizontal: false,
+	onRefresh: () => null
 }
 List.propTypes = {
 	data: PropTypes.array,
 	renderItem: PropTypes.func,
-	listEmptyComponent: PropTypes.object,
+	listHeader: PropTypes.node,
+	listFooter: PropTypes.node,
+	listEmpty: PropTypes.node,
 	contentContainerStyle: PropTypes.object,
 	isReturnScrollToStart: PropTypes.bool,
-	isHorizontal: PropTypes.bool
+	isHorizontal: PropTypes.bool,
+	onRefresh: PropTypes.func
 }
 
 export default List
