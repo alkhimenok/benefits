9403df332d52100626733e9ba4911c472ad1306e
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-05-07 02:32:04 +0300

    create: navigation constants

---
+++ src/constants/navigationRoutes.js
@@ -0,0 +1,9 @@
+const discountStack = 'discountStack'
+const favoritesStack = 'favoritesStack'
+const accountStack = 'accountStack'
+
+const discount = 'discount'
+const favorites = 'favorites'
+const account = 'account'
+
+export { discount, favorites, account, discountStack, favoritesStack, accountStack }
